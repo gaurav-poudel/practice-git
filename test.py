1a5881f4baa3d826dfb2fc5c40dee03d8781e085
Gaurav poudel <gauravpoudel83@gmail.com>
2020-12-12 15:54:52 +0545

    this is bubble short

--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-hello
-what
+def bubble_short(arr):
+    for i in range(len(arr)-1, 0, -1):
+        for j in range(i):
+            if arr[j] > arr[j+1]:
+                temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+            print(arr)
+
+bubble_short([1,3,6,4,2,9])
